bd3c4bf4056973f344b86f3f9803851b6783918b
ike04 <boc.0409@outlook.jp>
2020-11-08 12:03:56 +0900

    タブバーの実装

--- Tapioca milk tea/ContentView.swift
+++ Tapioca milk tea/ContentView.swift
@@ -3,7 +3,24 @@ import SwiftUI
 struct ContentView : View {
     
     var body: some View {
-        Text("Hello SwiftUI!")
+        TabView{
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("HOME")
+                }
+            OrderView()
+                .tabItem {
+                    Image(systemName: "textbox")
+                    Text("ORDER")
+                }
+            OrderHistoryView()
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("LIST")
+                }
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
